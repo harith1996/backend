42036a305b6de79f162c192e8a4f478757b5c1f0
Sam Martin Vargas Giagnocavo <samfefuty@hotmail.com>
2021-09-24 22:49:15 +0200

    Change GET to PUT for LED state

--- src/routes/led.js
+++ src/routes/led.js
@@ -5,7 +5,7 @@ const router = express.Router()
 /** GET /api/led to get the LED's state */
 router.get('/led', controller.getStateLED);
 
-/** GET /api/led/{0,1} to change the LED's state */
-router.get('/led/:state', controller.setStateLED);
+/** PUT /api/led/{0,1} to change the LED's state */
+router.put('/led/:state', controller.setStateLED);
 
 module.exports = router;
